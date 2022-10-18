aef1a7f3593ba665f0ff7a643834fe258c59e0bd
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-10-19 00:45:17 +0900

    ぐるぐるできるのは確認した。

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,30 @@
     windows_subsystem = "windows"
 )]
 
+use std::{thread, time::Duration};
+
+use mouse_rs::Mouse;
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn grugru() {
+    let mouse = Mouse::new();
+    let mut i = 0.0;
+    loop {
+        thread::sleep(Duration::from_millis(100));
+        let r = 50.0;
+        let x = f64::cos(i) * r + 500.0;
+        let y = f64::sin(i) * r + 500.0;
+        println!("x: {}, y: {}, i: {}", x, y, i);
+        mouse
+            .move_to(x as i32, y as i32)
+            .expect("Unable to move mouse");
+        i += 1.0;
+    }
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![grugru])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
